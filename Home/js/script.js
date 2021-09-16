220cd1f39a40ee2e07a89bd774e81911ebd53a86
Yashasvi Gunasekara <88892012+YashasviGunasekara@users.noreply.github.com>
2021-09-16 20:55:46 +0530

    Update script.js

--- Home/js/script.js
+++ Home/js/script.js
@@ -22,3 +22,18 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+
+
+
+
+/*____________Remove Menu - Mobile _______________*/
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+        // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
